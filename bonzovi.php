8e9836ab94a734b20db304d3eaeeaf667e5c951e
Himanshu Nautiyal <47600885+Himjo@users.noreply.github.com>
2020-08-03 17:19:05 +0530

    commit

---
+++ bonzovi.php
@@ -0,0 +1,10 @@
+<marquee> WECOME TO ANSIBLE AUTOMATION WORLD - BY MR. VIMAL DAGA </marquee> 
+<br />
+<pre>
+<?php
+echo'
+<center> <h2> Ansible -  Simplest way to automate apps and IT infrastructure. Application Deployment + Configuration Management + Continuous Delivery </h2> </center>
+';
+?>
+</pre>
+<img src = "https://raw.githubusercontent.com/Himjo/bonzovi/master/vimal-sir.jpg" >
